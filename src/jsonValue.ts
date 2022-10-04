4346b8281914579cb4d32b90ec61ae05cd7a3a3b
Hernan Pentimalli <hpentimalli@gmail.com>
2022-10-04 18:38:06 -0300

    Added JSONValue type check

--- src/jsonValue.ts
+++ src/jsonValue.ts
@@ -5,3 +5,37 @@ export type JSONValue =
   | null
   | { [x: string]: JSONValue }
   | Array<JSONValue>;
+
+export const assertJSONValue = (input: any): JSONValue => {
+  const type = typeof input;
+
+  if (
+    type === 'string' ||
+    type === 'number' ||
+    type === 'boolean' ||
+    input === null
+  ) {
+    return input;
+  }
+
+  if (type === 'object') {
+    if (Array.isArray(input)) {
+      return input.map(assertJSONValue);
+    } else {
+      const keys = Object.keys(input);
+
+      const obj = {} as any;
+
+      for (const key of keys) {
+        const val = input[key];
+        if (val !== undefined) {
+          obj[key] = assertJSONValue(input[key]);
+        }
+      }
+
+      return obj;
+    }
+  }
+
+  throw new Error(`Type ${type} not supported as JSONValue`);
+};
---
+++ src/tests/jsonValue.test.ts
@@ -0,0 +1,71 @@
+import { describe, it } from 'mocha';
+import * as chai from 'chai';
+import { assertJSONValue } from '../jsonValue';
+
+const expect = chai.expect;
+
+describe('assertJsonValue', () => {
+  it('asserts a string as valid', () => {
+    const val = assertJSONValue('foo');
+    expect(val).to.eq('foo');
+  });
+
+  it('asserts a number as valid', () => {
+    const val = assertJSONValue(123);
+    expect(val).to.eq(123);
+  });
+
+  it('asserts a boolean as valid', () => {
+    const val = assertJSONValue(true);
+    expect(val).to.eq(true);
+  });
+
+  it('asserts a null as valid', () => {
+    const val = assertJSONValue(null);
+    expect(val).to.be.null;
+  });
+
+  it('asserts an array of strings as valid', () => {
+    const val = assertJSONValue(['foo', 'boo', 'bah']);
+    expect(val).deep.eq(['foo', 'boo', 'bah']);
+  });
+
+  it('asserts an mixed object as valid', () => {
+    const val = assertJSONValue({ foo: 1, boo: 'bar' });
+    expect(val).deep.eq({ foo: 1, boo: 'bar' });
+  });
+
+  it('asserts deeply mixed object as valid', () => {
+    const val = assertJSONValue({ foo: 1, boo: 'bar', baz: { a: 1, b: null } });
+    expect(val).deep.eq({ foo: 1, boo: 'bar', baz: { a: 1, b: null } });
+  });
+
+  it('fails on symbol', () => {
+    expect(() => {
+      assertJSONValue(Symbol());
+    }).to.throw();
+  });
+
+  it('fails on function', () => {
+    expect(() => {
+      assertJSONValue(() => {});
+    }).to.throw();
+  });
+
+  it('fails on undefined', () => {
+    expect(() => {
+      assertJSONValue(undefined);
+    }).to.throw();
+  });
+
+  it('fails on undefined as array member', () => {
+    expect(() => {
+      assertJSONValue(['foo', undefined]);
+    }).to.throw();
+  });
+
+  it('strips away undefined values', () => {
+    const stripped = assertJSONValue({ foo: undefined, bar: 'baz' });
+    expect(stripped).deep.eq({ bar: 'baz' });
+  });
+});
